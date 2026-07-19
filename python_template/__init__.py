12bf3c26a45b38363d247bf718cea147c8c3deaf
Jinzhe Zeng <njzjz@qq.com>
2026-07-19 22:56:44 +0800

    Initial commit

---
+++ python_template/__init__.py
@@ -0,0 +1,11 @@
+"""A Python template (please revise this docstring)."""
+
+from __future__ import annotations
+
+from ._version import __version__
+
+__email__ = "jinzhe.zeng@ustc.edu.cn"
+
+__all__ = [
+    "__version__",
+]
